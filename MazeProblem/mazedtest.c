45d36492885121b9f841c8a8c83e3f658b605f8c
rougeLi <rouge_3877@outlook.com>
2023-12-02 11:16:02 +0800

    It seems to have only one question: Output file is bigger than the input file

--- MazeProblem/mazedtest.c
+++ MazeProblem/mazedtest.c
@@ -1,159 +1,78 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
-#include <unistd.h>
 
-#define MAX_ROW 10
-#define MAX_COL 10
-
-#define MAX_QUEUE_SIZE 100
-
-#define MAZE_PATH 0
-#define MAZE_WALL 1
-#define MAZE_VISITED 2
-
-#define GRAPH_EXIST_EDGE 1
-#define GRAPH_NO_EDGE 0
-
-typedef struct Edge {
-    int _row;
-    int _col;
-} Edge, *pEdge;
+#define MAX_ROW 3
+#define MAX_COL 3
 
 typedef struct Point {
-    int _x;
-    int _y;
-} Point, *pPoint;
+    int _row, _col;
+} Point;
 
-typedef struct Graph {
-    int _edgeNum;
-    int _vertexNum;
-    int **_adjMatrix;
-    pPoint *_vertexs;
-} Graph, *pGraph;
+typedef struct Wall {
+    int _index1, _index2;
+    bool _isWall;
+} Wall;
 
-typedef struct Maze {
-    int _row;
-    int _col;
-    pGraph _graph;
-} Maze, *pMaze;
-
-typedef struct Queue {
-    int _front;
-    int _rear;
-    int _size;
-    int _capacity;
-    int *_data;
-} Queue, *pQueue;
-
-pGraph createGraph(int vertexNum)
+Point index2point(int index, int row, int col)
 {
-    pGraph graph = (pGraph)malloc(sizeof(Graph));
-    graph->_edgeNum = 0;
-    graph->_vertexNum = vertexNum;
-    graph->_adjMatrix = (int **)malloc(sizeof(int *) * vertexNum);
-    graph->_vertexs = (pPoint *)malloc(sizeof(pPoint) * vertexNum);
-    for (int i = 0; i < vertexNum; i++) {
-        graph->_adjMatrix[i] = (int *)malloc(sizeof(int) * vertexNum);
-        graph->_vertexs[i] = (pPoint)malloc(sizeof(Point));
-        graph->_vertexs[i]->_x = i / 10;
-        graph->_vertexs[i]->_y = i % 10;
-        for (int j = 0; j < vertexNum; j++) {
-            graph->_adjMatrix[i][j] = 0;
+    Point p;
+    p._row = index / col;
+    p._col = index % col;
+    return p;
+}
+
+int point2index(Point p, int row, int col)
+{
+    return p._row * col + p._col;
+}
+
+int main(void)
+{
+    int Maze[MAX_ROW * MAX_COL];
+    bool Wall[MAX_ROW * MAX_COL][MAX_ROW * MAX_COL];
+    for (int i = 0; i < MAX_ROW * MAX_COL; i++) {
+        Maze[i] = i;
+        for (int j = 0; j < MAX_ROW * MAX_COL; j++) {
+            Wall[i][j] = 0;
         }
     }
-    return graph;
-}
 
-void printGraph(pGraph graph)
-{
-    int vertexNum = graph->_vertexNum;
-    int **adjMatrix = graph->_adjMatrix;
-    for (int i = 0; i < vertexNum; i++) {
-        for (int j = 0; j < vertexNum; j++) {
-            printf("%d ", adjMatrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void addEdge(pGraph graph, pEdge edge)
-{
-    int row = edge->_row;
-    int col = edge->_col;
-    graph->_adjMatrix[row][col] = GRAPH_EXIST_EDGE;
-    graph->_adjMatrix[col][row] = GRAPH_EXIST_EDGE;
-    graph->_edgeNum++;
-}
-
-void removeEdge(pGraph graph, pEdge edge)
-{
-    int row = edge->_row;
-    int col = edge->_col;
-    graph->_adjMatrix[row][col] = GRAPH_NO_EDGE;
-    graph->_adjMatrix[col][row] = GRAPH_NO_EDGE;
-    graph->_edgeNum--;
-}
-
-// 初始化迷宫
-pMaze initMaze(int row, int col)
-{
-    pMaze maze = (pMaze)malloc(sizeof(Maze));
-    maze->_row = row;
-    maze->_col = col;
-    maze->_graph = createGraph(row * col);
-    return maze;
-}
-
-// 打印迷宫，注意边界
-void printMaze(pMaze maze, FILE *fp)
-{
-    int row = maze->_row;
-    int col = maze->_col;
-    int **adjMatrix = maze->_graph->_adjMatrix;
-
-    for (int i = 0; i < row; i++) {
-        fprintf(fp, "+");
-        for (int j = 0; j < col; j++) {
-            fprintf(fp, "---+");
-        }
-        fprintf(fp, "\n");
-
-        fprintf(fp, "|");
-        for (int j = 0; j < col; j++) {
-            // 检查当前位置和右侧位置之间是否有边
-            if (j < col - 1 && adjMatrix[i * col + j][i * col + j + 1] == GRAPH_NO_EDGE) {
-                fprintf(fp, "   |");
-            } else {
-                fprintf(fp, "   ");
+    // draw the maze
+    for (int i = 0; i <= MAX_ROW; i++) {
+        // draw the upper wall
+        if (i == 0 || i == MAX_ROW) {
+            for (int j = 0; j < MAX_COL; j++) {
+                printf("+---");
             }
+            printf("+\n");
+        } else {
+            for (int j = 0; j < MAX_COL; j++) {
+                if (Wall[i * MAX_COL + j][i * MAX_COL + j - MAX_COL]) {
+                    printf("+   ");
+                } else {
+                    printf("+---");
+                }
+            }
+            printf("+\n");
         }
-        fprintf(fp, "|");
-        fprintf(fp, "\n");
+
+        // draw the left wall
+        if (i != MAX_ROW) {
+            for (int j = 0; j <= MAX_COL; j++) {
+                if (j == 0) {
+                    printf("|   ");
+                } else if (Wall[i * MAX_COL + j][i * MAX_COL + j + 1]) {
+                    printf("%d%d ", i * MAX_COL + j, i * MAX_COL + j + 1);
+                } else if (Wall[i * MAX_COL + j][i * MAX_COL + j - 1] == 0) {
+                    printf("|   ");
+                } else if (j == MAX_COL) {
+                    printf("|");
+                }
+            }
+            printf("\n");
+        }
     }
-
-    fprintf(fp, "+");
-    for (int j = 0; j < col; j++) {
-        fprintf(fp, "---+");
-    }
-    fprintf(fp, "\n");
-}
-
-// 图的Kruskal算法，最小生成树
-void kruskal(pGraph graph){
-    
-}
-
-
-// 测试迷宫的生成和打印（Kruskal算法）
-int main()
-{
-    srand(time(NULL));
-    pMaze maze = initMaze(MAX_ROW, MAX_COL);
-    createMazeInKruskal(maze);
-    printGraph(maze->_graph);
-    printMaze(maze, stdout);
     return 0;
 }
